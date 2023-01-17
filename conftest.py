733bbfe276e6953b4e5b6ff76953ad5eb5ea08c4
artyommatt <artyommatyushonok@gmail.com>
2023-01-17 12:18:50 +0300

    add parser. add login and password to secure.json.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
-from typing import Generator
+import json
+import os
+from typing import Generator, Dict, Any
 
 from _pytest.fixtures import fixture
 from playwright.sync_api import sync_playwright, Playwright
@@ -13,16 +15,29 @@ def get_playwright() -> Generator[Playwright, None, None]:
 
 
 @fixture
-def desktop_app(get_playwright: Playwright) -> Generator[App, None, None]:
-    app = App(get_playwright, base_url="http://127.0.0.1:8000")
+def desktop_app(get_playwright: Playwright, request: Any) -> Generator[App, None, None]:
+    base_url = request.config.getoption("--base_url")
+    app = App(get_playwright, base_url=base_url)
     app.goto('/')
     yield app
     app.close()
 
 
 @fixture
-def desktop_app_auth(desktop_app: App) -> Generator[App, None, None]:
-    app = desktop_app
-    app.goto("/login")
-    app.login("alice", "Qamania123")
-    yield app
+def desktop_app_auth(desktop_app: App, request: Any) -> Generator[App, None, None]:
+    secure = request.config.getoption("--secure")
+    config = load_config(secure)
+    desktop_app.goto("/login")
+    desktop_app.login(**config)
+    yield desktop_app
+
+
+def pytest_addoption(parser: Any) -> None:
+    parser.addoption("--base_url", action="store", default="http://127.0.0.1:8000")
+    parser.addoption("--secure", action="store", default="secure.json")
+
+
+def load_config(config_file: str) -> Dict[str, str]:
+    config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), config_file)
+    with open(config_file) as cfg:
+        return json.loads(cfg.read())
