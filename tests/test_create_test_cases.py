983bd647991b04a97262cf99ff21d0d2cf84ec00
artyommatt <artyommatyushonok@gmail.com>
2023-01-23 21:54:31 +0300

    add mark.parametraze to tests

--- tests/test_create_test_cases.py
+++ tests/test_create_test_cases.py
@@ -1,28 +1,24 @@
+from typing import Dict, List, Tuple
+
+from pytest import mark
+
 from page_object.application import App
 
+data: Dict[str, str | List[str | Tuple[str, str]]] = {
+    "argnames": "name, description",
+    "argvalues": [
+        ("hello", "world"),
+        ("hello", ""),
+        ("123", "world"),
+    ],
+    "ids": ["general", "without description", "digit name"]
+}
 
-def test_create_test_case(desktop_app_auth: App) -> None:
-    test_name = "hello"
+
+@mark.parametrize(**data)
+def test_create_test_case(desktop_app_auth: App, name: str, description: str) -> None:
     desktop_app_auth.navigate_to("Create new test")
-    desktop_app_auth.test_cases.create_test(test_name, "world")
+    desktop_app_auth.test_cases.create_test(name, description)
     desktop_app_auth.navigate_to("Test Cases")
-    assert desktop_app_auth.test_cases.check_test_exist(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
-
-
-def test_create_test_case_no_description(desktop_app_auth: App) -> None:
-    test_name = "hello"
-    desktop_app_auth.navigate_to("Create new test")
-    desktop_app_auth.test_cases.create_test(test_name, "")
-    desktop_app_auth.navigate_to("Test Cases")
-    assert desktop_app_auth.test_cases.check_test_exist(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
-
-
-def test_create_test_case_degits_name(desktop_app_auth: App) -> None:
-    test_name = "123"
-    desktop_app_auth.navigate_to("Create new test")
-    desktop_app_auth.test_cases.create_test(test_name, "world")
-    desktop_app_auth.navigate_to("Test Cases")
-    assert desktop_app_auth.test_cases.check_test_exist(test_name)
-    desktop_app_auth.test_cases.delete_test_by_name(test_name)
+    assert desktop_app_auth.test_cases.check_test_exist(name)
+    desktop_app_auth.test_cases.delete_test_by_name(name)
